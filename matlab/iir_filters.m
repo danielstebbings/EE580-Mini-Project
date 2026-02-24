664d7f1ad9fd2d855da810519e8b828da074f806
daniel <daniel>
2026-02-24 15:29:47 +0000

    IIR w. 10 coeffs

---
+++ matlab/iir_filters.m
@@ -0,0 +1,12 @@
+%[text] ## Creating IIR Filters
+%[text] 
+fs = single(8e3);
+
+fc_low  = fs/6;
+fc_high = 2*fs/6; 
+
+%[appendix]{"version":"1.0"}
+%---
+%[metadata:view]
+%   data: {"layout":"onright"}
+%---
